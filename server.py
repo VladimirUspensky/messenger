baa91244728ddbb35fc296173932f81e872ee9dd
Vladimir <vl.shev@list.ru>
2020-10-28 22:02:39 +0300

    add first version of private messages on the server side

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import sys
 import socket
 import asyncio
 
@@ -7,7 +8,7 @@ class Server:
     def __init__(self, ip: str, port: int):
         self.ip = str(ip)
         self.port = int(port)
-        self.all_clients = set()
+        self.all_clients = []
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server.bind((self.ip, self.port))
@@ -21,18 +22,38 @@ class Server:
         """Connects clients to the server"""
         while True:
             client_socket, _ = await self.event_loop.sock_accept(self.server)
-            self.all_clients.add(client_socket)
-            self.event_loop.create_task(self.send_message(client_socket))
+            if client_socket not in self.all_clients:
+                self.all_clients.append(client_socket)
+            self.event_loop.create_task(self.get_client_message(client_socket))
 
-    async def send_message(self, client_socket: socket.socket) -> None:
+    async def send_message_to_everyone(self, client_socket: socket.socket, message: bytes) -> None:
         """Sends message from one client to others"""
+        while True:
+            for client in self.all_clients:
+                if client is not client_socket:
+                    await self.event_loop.sock_sendall(client, message)
+            break
+
+    async def get_client_message(self, client_socket: socket.socket) -> None:
+        """Gets client's message"""
         while True:
             message = await self.event_loop.sock_recv(client_socket, 1024)
             if not message:
                 break
-            for client in self.all_clients:
-                if client is not client_socket:
-                    await self.event_loop.sock_sendall(client, message)
+            split_message = message.decode('utf-8').split(' ')
+            if split_message[len(split_message) - 2] == '-to':
+                await self.send_message_by_id((split_message[0] + '\n\r').encode('utf-8'),
+                                              int(split_message[len(split_message) - 1]))
+            else:
+                await self.send_message_to_everyone(client_socket, message)
+
+    async def send_message_by_id(self, message: bytes, client_id: int) -> None:
+        """Sends message from one client to another one, by the name of the second"""
+        while True:
+            for client_i in range(len(self.all_clients)):
+                if client_i == client_id:
+                    await self.event_loop.sock_sendall(self.all_clients[client_i], message)
+            break
 
     async def start(self) -> None:
         """Starts the server"""
